08cd606db4425a728a58b64356a4191253edcdd4
Dhika Rizky <deka_r@outlook.com>
2019-09-20 19:08:39 +0700

    Build

---
+++ api/stations/tangerang.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+const stations = ['Duri', 'Grogol', 'Pesing', 'Taman Kota', 'Bojong Indah', 'Rawa Buaya', 'Kalideres', 'Poris', 'Batuceper', 'Tanah Tinggi', 'Tangerang'];
+var _default = stations;
+exports.default = _default;
