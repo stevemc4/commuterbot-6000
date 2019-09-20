8cbff00ca90dbe4232b1981d3ae246ea603178d0
Dhika Rizky <deka_r@outlook.com>
2019-09-21 00:36:50 +0700

    Added new lines

---
+++ src/stations/bogor.js
@@ -0,0 +1,28 @@
+const stations = [
+  'Jakartakota',
+  'Jayakarta',
+  'Mangga Besar',
+  'Sawah Besar',
+  'Juanda',
+  'Gondangdia',
+  'Cikini',
+  'Manggarai',
+  'Tebet',
+  'Cawang',
+  'Duren Kalibata',
+  'Pasar Minggu Baru',
+  'Pasar Minggu',
+  'Tanjung Barat',
+  'Lenteng Agung',
+  'Universitas Pancasila',
+  'Universitas Indonesia',
+  'Pondok Cina',
+  'Depok Baru',
+  'Depok',
+  'Citayam',
+  'Bojonggede',
+  'Cilebut',
+  'Bogor'
+]
+
+export default [stations, 'ED1B24', 'Bogor']
--- src/stations/index.js
+++ src/stations/index.js
@@ -2,12 +2,16 @@ import BEKASI_LINE from './bekasi'
 import LOOP_LINE from './loop'
 import RANGKASBITUNG_LINE from './rangkasbitung'
 import TANGERANG_LINE from './tangerang'
+import TANJUNG_PRIOK_LINE from './tanjungpriok'
+import BOGOR_LINE from './bogor'
 
 const lines = [
   BEKASI_LINE,
   LOOP_LINE,
   RANGKASBITUNG_LINE,
-  TANGERANG_LINE
+  TANGERANG_LINE,
+  TANJUNG_PRIOK_LINE,
+  BOGOR_LINE
 ]
 
 export default lines
