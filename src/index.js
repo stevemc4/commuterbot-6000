df1640efb9fde81a6dfcd01f8f6db23d6013081d
Dhika Rizky <deka_r@outlook.com>
2019-09-20 18:37:35 +0700

    Added text variation

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import stations from './stations'
 function emitText () {
   const line = stations[Math.floor(Math.random() * stations.length)]
   const station = line[Math.floor(Math.random() * line.length)]
+  
+  const announcementLines = [
+    `Sesaat lagi Anda akan tiba di Stasiun ${station}. Pastikan tiket dan barang bawaan Anda tidak tertinggal dan perhatikan celah peron.`,
+    `Stasiun berikutnya, Stasiun ${station}.`,
+    `Stasiun ${station}. Hati-hati melangkah.`
+  ]
 
-  return `Sesaat lagi Anda akan tiba di Stasiun ${station}.
-Pastikan tiket dan barang bawaan Anda tidak tertinggal dan perhatikan celah peron.`
+  return announcementLines[Math.floor(Math.random() * announcementLines.length)]
 }
 
 export default async function (_req, res) {
