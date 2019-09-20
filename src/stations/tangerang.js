2f59ae6ec41260b15edc4fea647ecb70a07a08e6
Dhika Rizky <deka_r@outlook.com>
2019-09-20 18:46:53 +0700

    Added more lines

---
+++ src/stations/tangerang.js
@@ -0,0 +1,15 @@
+const stations = [
+  'Duri',
+  'Grogol',
+  'Pesing',
+  'Taman Kota',
+  'Bojong Indah',
+  'Rawa Buaya',
+  'Kalideres',
+  'Poris',
+  'Batuceper',
+  'Tanah Tinggi',
+  'Tangerang'
+]
+
+export default stations
