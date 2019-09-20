a9dfd11814a974f042a76dc1a3afba686bc7eef3
Dhika Rizky <deka_r@outlook.com>
2019-09-20 20:43:41 +0700

    More update to image styling

--- api/image.js
+++ api/image.js
@@ -31,14 +31,14 @@ async function _default(req, res) {
   function createStationNode(x, y, isCurrentStation = false) {
     ctx.strokeStyle = '#1565C0';
     ctx.beginPath();
-    ctx.ellipse(x, y, 48, 48, 0, 0, 360);
-    ctx.lineWidth = 24;
+    ctx.ellipse(x, y, 64, 64, 0, 0, 360);
+    ctx.lineWidth = 32;
     ctx.stroke();
 
     if (isCurrentStation) {
       ctx.closePath();
       ctx.beginPath();
-      ctx.ellipse(x, y, 24, 24, 0, 0, 360);
+      ctx.ellipse(x, y, 32, 32, 0, 0, 360);
       ctx.fillStyle = '#1565C0';
       ctx.fill();
     }
@@ -59,8 +59,8 @@ async function _default(req, res) {
   }
 
   function writeStationName(text, x, y, smaller = false) {
-    ctx.font = '32px Nunito';
-    if (smaller) ctx.font = '24px Nunito';
+    ctx.font = '48px Nunito';
+    if (smaller) ctx.font = '32px Nunito';
     const textMeasurement = ctx.measureText(text);
     const XPOS = x - textMeasurement.width / 2;
     ctx.fillStyle = '#1565C0';
@@ -71,14 +71,16 @@ async function _default(req, res) {
   ctx.fillStyle = '#ffffff';
   ctx.fillRect(0, 0, 1200, 630);
   ctx.restore();
-  const CENTER_NODE_COORDINATE = createPoint(WIDTH / 2, HEIGHT / 2);
-  const LEFT_NODE_COORDINATE = createPoint(WIDTH / 2 - 256 - 96, HEIGHT / 2);
-  const RIGHT_NODE_COORDINATE = createPoint(WIDTH / 2 + 256 + 96, HEIGHT / 2);
+  const CENTER_NODE_COORDINATE = createPoint(WIDTH / 2, HEIGHT / 2 - 72);
+  const LEFT_NODE_COORDINATE = createPoint(WIDTH / 2 - 384 - 96, HEIGHT / 2 - 72);
+  const RIGHT_NODE_COORDINATE = createPoint(WIDTH / 2 + 384 + 96, HEIGHT / 2 - 72);
   createStationNode(CENTER_NODE_COORDINATE.x, CENTER_NODE_COORDINATE.y, true);
   createStationNode(LEFT_NODE_COORDINATE.x, LEFT_NODE_COORDINATE.y);
   createStationNode(RIGHT_NODE_COORDINATE.x, RIGHT_NODE_COORDINATE.y);
   createLine(LEFT_NODE_COORDINATE.x + 48, LEFT_NODE_COORDINATE.y, CENTER_NODE_COORDINATE.x - 48, CENTER_NODE_COORDINATE.y);
+  createLine(LEFT_NODE_COORDINATE.x - 48, LEFT_NODE_COORDINATE.y, 0, LEFT_NODE_COORDINATE.y);
   createLine(RIGHT_NODE_COORDINATE.x - 48, RIGHT_NODE_COORDINATE.y, CENTER_NODE_COORDINATE.x + 48, CENTER_NODE_COORDINATE.y);
+  createLine(RIGHT_NODE_COORDINATE.x + 48, RIGHT_NODE_COORDINATE.y, WIDTH, RIGHT_NODE_COORDINATE.y);
   writeStationName(query.station, CENTER_NODE_COORDINATE.x, CENTER_NODE_COORDINATE.y + 144);
   writeStationName(query.before, LEFT_NODE_COORDINATE.x, LEFT_NODE_COORDINATE.y + 144, true);
   writeStationName(query.after, RIGHT_NODE_COORDINATE.x, RIGHT_NODE_COORDINATE.y + 144, true);
--- src/image.js
+++ src/image.js
@@ -20,13 +20,13 @@ export default async function (req, res) {
   function createStationNode (x, y, isCurrentStation = false) {
     ctx.strokeStyle = '#1565C0'
     ctx.beginPath()
-    ctx.ellipse(x, y, 48, 48, 0, 0, 360)
-    ctx.lineWidth = 24
+    ctx.ellipse(x, y, 64, 64, 0, 0, 360)
+    ctx.lineWidth = 32
     ctx.stroke()
     if (isCurrentStation) {
       ctx.closePath()
       ctx.beginPath()
-      ctx.ellipse(x, y, 24, 24, 0, 0, 360)
+      ctx.ellipse(x, y, 32, 32, 0, 0, 360)
       ctx.fillStyle = '#1565C0'
       ctx.fill()
     }
@@ -46,8 +46,8 @@ export default async function (req, res) {
   }
 
   function writeStationName (text, x, y, smaller = false) {
-    ctx.font = '32px Nunito'
-    if (smaller) ctx.font = '24px Nunito'
+    ctx.font = '48px Nunito'
+    if (smaller) ctx.font = '32px Nunito'
     const textMeasurement = ctx.measureText(text)
     const XPOS = x - textMeasurement.width / 2
     ctx.fillStyle = '#1565C0'
@@ -60,15 +60,19 @@ export default async function (req, res) {
 
   ctx.restore()
 
-  const CENTER_NODE_COORDINATE = createPoint(WIDTH / 2, HEIGHT / 2)
-  const LEFT_NODE_COORDINATE = createPoint(WIDTH / 2 - 256 - 96, HEIGHT / 2)
-  const RIGHT_NODE_COORDINATE = createPoint(WIDTH / 2 + 256 + 96, HEIGHT / 2)
+  const CENTER_NODE_COORDINATE = createPoint(WIDTH / 2, HEIGHT / 2 - 72)
+  const LEFT_NODE_COORDINATE = createPoint(WIDTH / 2 - 384 - 96, HEIGHT / 2 - 72)
+  const RIGHT_NODE_COORDINATE = createPoint(WIDTH / 2 + 384 + 96, HEIGHT / 2 - 72)
 
   createStationNode(CENTER_NODE_COORDINATE.x, CENTER_NODE_COORDINATE.y, true)
   createStationNode(LEFT_NODE_COORDINATE.x, LEFT_NODE_COORDINATE.y)
   createStationNode(RIGHT_NODE_COORDINATE.x, RIGHT_NODE_COORDINATE.y)
+
   createLine(LEFT_NODE_COORDINATE.x + 48, LEFT_NODE_COORDINATE.y, CENTER_NODE_COORDINATE.x - 48, CENTER_NODE_COORDINATE.y)
+  createLine(LEFT_NODE_COORDINATE.x - 48, LEFT_NODE_COORDINATE.y, 0, LEFT_NODE_COORDINATE.y)
+
   createLine(RIGHT_NODE_COORDINATE.x - 48, RIGHT_NODE_COORDINATE.y, CENTER_NODE_COORDINATE.x + 48, CENTER_NODE_COORDINATE.y)
+  createLine(RIGHT_NODE_COORDINATE.x + 48, RIGHT_NODE_COORDINATE.y, WIDTH, RIGHT_NODE_COORDINATE.y)
 
   writeStationName(query.station, CENTER_NODE_COORDINATE.x, CENTER_NODE_COORDINATE.y + 144)
   writeStationName(query.before, LEFT_NODE_COORDINATE.x, LEFT_NODE_COORDINATE.y + 144, true)
